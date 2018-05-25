a847e1182170a5810e9a5ea21ce6026d8b8c78b7
Omar Jbara <omar.Jbara2@gmail.com>
2018-05-25 10:47:08 +0200

    Removed useless components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,69 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app" class="App">
+        <p class="my-component">
+            Je suis mon premier composant
+        </p>
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                {{ todo.name }} - {{ todo.dueDate.toLocaleString() }}
+            </li>
+        </ul>
+        <input type="text" v-model="newTodo" @keyup.enter="addTodo" placeholder="Entrez votre todo...">
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    li {
+        font-size: 2rem;
+        color: red;
+        &:hover {
+            color: blue;
+        }
     }
-  }
-}
 </style>
+
+<script>
+    export default {
+        name: 'app',
+        methods: {
+            addTodo () {
+                if (this.newTodo === '') return
+                const id = this.todos[this.todos.length - 1].id + 1
+                const todo = {
+                    id,
+                    name: this.newTodo,
+                    dueDate: new Date()
+                };
+                this.todos.push(todo)
+                this.newTodo = ''
+            }
+        },
+        data () {
+            return {
+                newTodo: '',
+                todos: [
+                    {
+                        id: 1,
+                        name: 'Todo 1',
+                        dueDate: new Date()
+                    },
+                    {
+                        id: 2,
+                        name: 'Todo 2',
+                        dueDate: new Date()
+                    },
+                    {
+                        id: 3,
+                        name: 'Todo 3',
+                        dueDate: new Date()
+                    },
+                    {
+                        id: 4,
+                        name: 'Todo 4',
+                        dueDate: new Date()
+                    }
+                ]
+            }
+        }
+    }
+</script>
